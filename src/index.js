2131e381186bf35daa5b3e37b2e3a892150ebb7e
Dariiob <92950768+Dariiob@users.noreply.github.com>
2021-12-06 20:05:39 -0500

    fix switch import in routing.jsx

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-app.use(express.static(__dirname)); //here is important thing - no static directory, because all static :)
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
